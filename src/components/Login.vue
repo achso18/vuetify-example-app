a136c2dcf64405184e687103f8673228695b87ed
Achim Geisler <achim.geisler@nagarro-es.com>
2023-06-09 19:51:58 +0200

    initial commit

---
+++ src/components/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container>
+    <v-card>
+      <v-card-title>Login</v-card-title>
+      <v-card-text>
+        <v-form @submit.prevent="login">
+          <v-text-field v-model="username" label="Username"></v-text-field>
+          <v-text-field
+            v-model="password"
+            label="Password"
+            type="password"
+          ></v-text-field>
+          <v-btn type="submit">Login</v-btn>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script setup>
+//
+</script>
---
+++ src/layouts/default/AppBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app-bar :elevation="10">
+    <v-app-bar-title>
+      {{ appTitle }}
+    </v-app-bar-title>
+    <v-toolbar-items class="hidden-xs-only">
+      <v-btn flat v-for="item in menuItems" :key="item.title" :to="item.path">
+        <v-icon left dark>{{ item.icon }}</v-icon>
+        {{ item.title }}
+      </v-btn>
+    </v-toolbar-items>
+    <v-spacer></v-spacer>
+    <v-btn icon="mdi-login" :key="'login'" :to="'/login'"></v-btn>
+  </v-app-bar>
+</template>
+
+<script setup>
+const sidebar = false;
+const menuItems = [{ title: "Home", icon: "mdi-home", path: "/" }];
+const appTitle = "My App";
+</script>
